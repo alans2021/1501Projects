48370f19ec7f31f7ddd3037f6cf4504a8ea7dceb
alans2021 <als432@pitt.edu>
2018-09-06 12:38:51 -0400

    Read input to make crossword board

--- Assignment1/code/Crossword.java
+++ Assignment1/code/Crossword.java
@@ -1,14 +1,43 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
 public class Crossword{
 	
-	public static void main(String[]args){
+	public static void main(String[]args) throws IOException{
 		DictInterface dict = new MyDictionary();
 		Scanner dictScan = new Scanner(new FileInputStream("dict8.txt"));
 		while (dictScan.hasNext()){
-			word = dictScan.nextLine();
+			String word = dictScan.nextLine();
 			dict.add(word);
 		}
 
 		String board = args[0];
-		Scanner boardScan = new Scanner(new FileInputStream(board));		
+		Scanner boardScan = new Scanner(new FileInputStream(board));
+		String test = boardScan.nextLine();
+		int size = Integer.parseInt(test);
+		String[][] Crossword = new String[size][size];
+
+		for (int i = 0; i < size; i++){
+			String line = boardScan.nextLine();
+			for (int j = 0; j < size; j++){
+				String charac = line.substring(j, j + 1);
+				if (charac.equals("+"))
+					Crossword[i][j] = "+";
+				else if (charac.equals("-"))
+					Crossword[i][j] = "-";
+				else
+					Crossword[i][j] = charac;
+			}
+
+		}
+
+		for (int i = 0; i < size; i++){
+			for (int j = 0; j < size; j++)
+				System.out.print(Crossword[i][j]);
+			System.out.println();
+		}
+
+
 	}
 }
