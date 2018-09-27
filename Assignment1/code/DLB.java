90b71594b55b9032ded449a51b0018220c8e0497
alans2021 <als432@pitt.edu>
2018-09-26 23:36:43 -0400

    Implement DLB

--- Assignment1/code/DLB.java
+++ Assignment1/code/DLB.java
@@ -1,12 +1,11 @@
 import java.util.*;
 
 public class DLB implements DictInterface{
-	private ArrayList<String> list;
 	private Node first;
 
 	public DLB()
 	{
-		list = new ArrayList<String>();
+		first = new Node();
 	}
 
 	// Add new String to end of list.  If String should come before
@@ -15,9 +14,19 @@ public class DLB implements DictInterface{
 	// DictInterface to make searches a bit faster.
 	public boolean add(String s)
 	{
+		Node curr = first;
 		for (int i = 0; i < s.length(); i++){
-			char letter = s.charAt(i); 
+			char letter = s.charAt(i);
+			
+			Node node = curr.search(letter);
+			if(node == null){
+				node = new Node(letter);
+				curr.addChild(node);
+			}
+			curr = node;			
 		}
+		curr.setWord();
+		
 		return true;
 	}
 
@@ -30,26 +39,97 @@ public class DLB implements DictInterface{
 
 	public int searchPrefix(StringBuilder s, int start, int end)
 	{
-		return 0;
+		Node curr = first;
+		for(int i = 0; i < s.length(); i++){
+			char letter = s.charAt(i);
+			curr = curr.search(letter);
+			if(curr == null)
+				return 0;
+		}
+		if(!curr.isWord())
+			return 1;
+		else if(curr.children.size() == 0)
+			return 2;
+		else
+			return 3;
 	}
 
 	private class Node
 	{
 		private char data;
-		private Node next;
-		private boolean value;
+		public LinkedList<Node> children;
+		public Node left;
+		public Node right;
+		private boolean value; //Indicates as a word
 
+		public Node(){
+			value = false;
+			children = new LinkedList<Node>();
+			left = null;
+			right = null;
+		}
+		
 		public Node(char c)
 		{
 			data = c;
-			next = null;
+			value = false;
+			children = new LinkedList<Node>();
+			left = null;
+			right = null;
+		}
+		
+		public char getChar(){
+			return data;
 		}
 
-		public Node(char c, Node n)
+		public void addChild(Node n)
 		{
-			data = c;
-			next = n;
+			if (children.size() > 1)
+			{
+				Node curr = children.get(0);
+				int i = 0;
+				while(curr != null && n.getChar() > curr.getChar()){
+					curr = curr.right;
+					i++;
+				}
+				children.add(i, n);
+				addSibling(n, curr);
+			}
+			else
+				children.add(n);
 		}
+		
+		private void addSibling(Node n, Node r){
+			Node left;
+			if (r != null){
+				left = r.left;
+				r.left = n;
+			}
+			else
+				left = children.get(children.size() - 2);
+			n.right = r;
+			n.left = left;
+			if (left != null)
+				left.right = n;	
+		}
+		
+		public Node search(char c)
+		{
+			for(int i = 0; i < children.size(); i++){
+				if(children.get(i).getChar() == c)
+					return children.get(i);
+			}
+			return null;
+		}
+		
+		public void setWord(){
+			value = true;
+		}
+		
+		public boolean isWord(){
+			return value;
+		}
+		
 	}
 }
 
