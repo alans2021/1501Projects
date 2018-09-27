a5982db74cfff0d5424ee2f5ce8bc2d4d4f5748f
alans2021 <als432@pitt.edu>
2018-09-26 23:37:08 -0400

    Preparing for both tasks

--- Assignment1/code/Crossword.java
+++ Assignment1/code/Crossword.java
@@ -6,23 +6,30 @@ public class Crossword{
 	
 	public static void main(String[]args) throws IOException{
 		DictInterface dict = new MyDictionary();
+		DictInterface DLB = new DLB();
 		Scanner dictScan = new Scanner(new FileInputStream("dict8.txt"));
 		while (dictScan.hasNext()){
 			String word = dictScan.nextLine();
 			dict.add(word);
+			DLB.add(word);
 		}
 
 		// Create Board object
-		String board = "test6a.txt";
+		String board = "test8c.txt";
 		Scanner boardScan = new Scanner(new FileInputStream(board));
 		int size = Integer.parseInt( boardScan.nextLine() );
-		Board crossword = new Board(size, boardScan, dict);
-
-		crossword.initBoard();
-		crossword.solveBoard(0, 0);
-		crossword.printSolution();
 		
-
+//		Board crossword = new Board(size, boardScan, dict);
+//		System.out.println("Solving board " + board + " using MyDictionary implementation");
+//		crossword.initBoard();
+//		crossword.solveBoard(0, 0);
+//		crossword.printSolution(true);
+		
+		Board crosswordAll = new Board(size, boardScan, DLB);
+		System.out.println("Solving board " + board + " using DLB implementation");
+		crosswordAll.initBoard();
+		crosswordAll.solveBoard(0, 0);
+		crosswordAll.printSolution(true);
 
 	}
 }
