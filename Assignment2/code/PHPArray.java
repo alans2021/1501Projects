76b601479f6ac39c157e444905eb169f9d137eaa
alans2021 <als432@pitt.edu>
2018-10-19 03:03:04 -0400

    Implemented all methods

--- Assignment2/code/PHPArray.java
+++ Assignment2/code/PHPArray.java
@@ -4,7 +4,7 @@ import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
-public class PHPArray<V> implements Iterable<V> {
+public class PHPArray<V> implements Iterable<V>{
 
 	private int size;
 	private Object[] values;
@@ -18,14 +18,15 @@ public class PHPArray<V> implements Iterable<V> {
 		values = new Object[i];
 	}
 	
-	private int hash(String key) {
+	private int hash(String key) { //Get hash value
 		return (key.hashCode() & 0x7fffffff) % (values.length);
     }
 	
-	private void resize(Object[] val){
+	private void resize(Object[] val){ //Double hash table length
+		System.out.println("\t\tSize: " + size + " -- resizing array from " + val.length + " to " + 2 * val.length);
 		values = new Object[2 * val.length];
 		Node curr = first;
-		while(curr != null){
+		while(curr != null){ //Rehash all nodes
 			int newHash = hash(curr.key);
 			while(values[newHash] != null)
 				newHash = (newHash + 1) % values.length;
@@ -36,28 +37,35 @@ public class PHPArray<V> implements Iterable<V> {
 	}
 
 	public void put(Object key, Object value){
-		if(size >= values.length / 2)
+		if(size >= values.length / 2) //Resize if array length less than double the size
 			resize(values);
 		
 		String inter =  new String(key.toString());
 		int hashCode = hash(inter);
-		while(values[hashCode] != null)
-			hashCode = (hashCode + 1) % (values.length);
 		
-		Node keyVal = new Node(inter, (V) value);
+		while(values[hashCode] != null){
+			Node temp = (Node) values[hashCode];
+			if(temp.key.equals(inter)){ //See if key already exists, if so, just modify value
+				temp.value = (V) value;
+				return;
+			}
+			hashCode = (hashCode + 1) % (values.length);
+		}
+		
+		Node keyVal = new Node(inter, (V) value); //Add node to hash table
 		values[hashCode] = keyVal;
 		if(current == null){
-			first = keyVal;
+			first = keyVal; // Set node relationships  
 			current = keyVal;
 		}
 		else{
-			current.next = keyVal;
+			current.next = keyVal; //Set next and prev relationship
 			keyVal.prev = current;
 			current = keyVal;
 		}
 		last = keyVal;
 		
-		size++;
+		size++; 
 		
 	}
 	
@@ -80,21 +88,33 @@ public class PHPArray<V> implements Iterable<V> {
             hashValue = (hashValue + 1) % values.length;
             temp = (Node) values[hashValue];
         }
-		
-		Node delete = temp;
-		Node previous = delete.prev;
-		previous.next = delete.next;
+		if(temp == first)
+			first = first.next;
+		else{
+			Node delete = temp;
+			Node previous = delete.prev;
+			previous.next = delete.next;
+		}
 		values[hashValue] = null;
 		
 		hashValue = (hashValue + 1) % values.length;
 		while(values[hashValue] != null){ //Rehash those in same cluster
 			temp = (Node) values[hashValue];
-			String keyRehash = temp.key;
-			Object valRehash = temp.value;
 			values[hashValue] = null;
-			put(keyRehash, valRehash);
+			System.out.println("\t\tKey " + temp.key + " rehashed ");
+			System.out.println();
+			reput(temp);
 			hashValue = (hashValue + 1) % values.length;
-		}	
+		}
+		size--;
+	}
+	
+	private void reput(Node temp){
+		int rehash = hash(temp.key);
+		while(values[rehash] != null){
+			rehash = (rehash + 1) % values.length;
+		}
+		values[rehash] = temp;
 	}
 	
 	public ArrayList<String> keys(){
@@ -130,17 +150,156 @@ public class PHPArray<V> implements Iterable<V> {
 				
 		}
 	}
-	
+
 	public void sort(){
+		try{
+			Comparable[] vals = new Comparable[size];
+			Node curr = first;
+			int i = 0;
+			while(curr != null){ //Put values in comparable array
+				vals[i] = (Comparable) curr.value; //Try to cast
+				curr = curr.next;
+				i++;
+			}
+			vals = mergeSort(vals, 0, vals.length);
+			for(i = 0; i < values.length; i++)
+				values[i] = null;
+			first = null;
+			current = null;
+			last = null;
+			size = 0;
+			
+			for(int j = 0; j < vals.length; j++)
+				put(Integer.toString(j), vals[j]);
+			
+		}
+		catch(ClassCastException e){
+			System.out.println("PHPArray values are not Comparable -- cannot be sorted");
+		}
+	}
+	
+	private Comparable[] mergeSort(Comparable[] c, int start, int end){
+		if((end - start) == 1){
+			Comparable[] inter = {c[start]};
+			return inter;
+		}
+		Comparable[] left = mergeSort(c, start, (start + end) / 2);
+		Comparable[] right = mergeSort(c, (start + end) / 2, end);
+		Comparable[] inter = new Comparable[end - start];
+		int leftCurr = 0;
+		int leftEnd = left.length;
+		int rightCurr = 0;
+		int rightEnd = right.length;
+		int mergeIndex = 0;
+		while(leftCurr < leftEnd && rightCurr < rightEnd){
+			if(left[leftCurr].compareTo(right[rightCurr]) <= 0){
+				inter[mergeIndex] = left[leftCurr];
+				leftCurr++;
+			}
+			else{
+				inter[mergeIndex] = right[rightCurr];
+				rightCurr++;
+			}
+			mergeIndex++;
+		}
+		if(leftCurr == leftEnd){
+			for(int i = rightCurr; i < rightEnd; i++){
+				inter[mergeIndex] = right[i];
+				mergeIndex++;
+			}
+		}
+		else{
+			for(int i = leftCurr; i < leftEnd; i++){
+				inter[mergeIndex] = left[i];
+				mergeIndex++;
+			}
+		}
+		return inter;
 		
 	}
 	
 	public void asort(){
+		try{
+			Object[] nodes = new Object[size];
+			Node curr = first;
+			int i = 0;
+			while(curr != null){
+				nodes[i] = curr;
+				curr = curr.next;
+				i++;
+			}
+			nodes = nodeSort(nodes, 0, nodes.length);
+			
+			for(i = 0; i < values.length; i++)
+				values[i] = null;
+			first = null;
+			current = null;
+			last = null;
+			size = 0;
+			
+			for(int j = 0; j < nodes.length; j++){
+				curr = (Node) nodes[j];
+				put(curr.key, curr.value);
+			}
+			
+		}
+		catch(ClassCastException e){
+			System.out.println("PHPArray values are not Comparable -- cannot be sorted");
+		}
+	}
+	
+	private Object[] nodeSort(Object[] c, int start, int end){
+		if((end - start) == 1){
+			Object[] inter = {c[start]};
+			return inter;
+		}
+		Object[] left = nodeSort(c, start, (start + end) / 2);
+		Object[] right = nodeSort(c, (start + end) / 2, end);
+		Object[] inter = new Object[end - start];
+		int leftCurr = 0;
+		int leftEnd = left.length;
+		int rightCurr = 0;
+		int rightEnd = right.length;
+		int mergeIndex = 0;
+		while(leftCurr < leftEnd && rightCurr < rightEnd){
+			if(((Node)left[leftCurr]).compareTo((Node)right[rightCurr]) <= 0){
+				inter[mergeIndex] = left[leftCurr];
+				leftCurr++;
+			}
+			else{
+				inter[mergeIndex] = right[rightCurr];
+				rightCurr++;
+			}
+			mergeIndex++;
+		}
+		if(leftCurr == leftEnd){
+			for(int i = rightCurr; i < rightEnd; i++){
+				inter[mergeIndex] = right[i];
+				mergeIndex++;
+			}
+		}
+		else{
+			for(int i = leftCurr; i < leftEnd; i++){
+				inter[mergeIndex] = left[i];
+				mergeIndex++;
+			}
+		}
+		return inter;
 		
 	}
 	
-	public PHPArray<String> array_flip(){
-		return null;
+	public PHPArray<String> array_flip() throws ClassCastException{
+		PHPArray<String> flip = new PHPArray<String>(values.length);
+		
+		Node curr = first;
+		while(curr != null){
+			String newKey = (String) curr.value;
+			String newVal = curr.key;
+			flip.put(newKey, newVal);
+			curr = curr.next;
+		}
+		
+		return flip;
 	}
 	
 	public static class Pair<V> {
@@ -189,6 +348,12 @@ public class PHPArray<V> implements Iterable<V> {
 		{
 			key = k;
 			value = val;
+		}
+
+		public int compareTo(Node n) throws ClassCastException{
+			Comparable first = (Comparable) this.value;
+			Comparable second = (Comparable) n.value;
+			return first.compareTo(second);
 		}
 
 	}
