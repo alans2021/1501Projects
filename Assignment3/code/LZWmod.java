2f03add968f6bedc0f337ce69cc0cce2d8ef7a47
alans2021 <als432@pitt.edu>
2018-11-07 02:57:12 -0500

    Part 2 finished

--- Assignment3/code/LZWmod.java
+++ Assignment3/code/LZWmod.java
@@ -3,8 +3,8 @@ import java.util.NoSuchElementException;
 public class LZWmod {
 
     private static final int R = 256;        // number of input chars
-    private static final int L = 4096;       // number of codewords = 2^W
-    private static final int W = 12;         // codeword width
+    private static final int L = 65536;      // number of codewords = 2^16, max size
+    private static int W = 9;         // codeword width, 9 at first
 
     public static void compress() {
         DLB<Integer> dict = new DLB<Integer>();
@@ -17,39 +17,39 @@ public class LZWmod {
         int code = R+1;  // R is codeword for EOF
 
         StringBuilder prefix = new StringBuilder();
-        char byteinput = BinaryStdIn.readChar(8);
+        char byteinput = BinaryStdIn.readChar(8); //Read character from input file
         while(true) {
             try{
-//                System.out.println(code + " ");
-//                if(code == 3040)
-//                    System.out.println("Testing");
-                prefix.append(byteinput);
-                int search = dict.searchPrefix(prefix);
-                while(search == 2) {
-                    prefix.append(BinaryStdIn.readChar(8));
-                    search = dict.searchPrefix(prefix);
+                prefix.append(byteinput); //Add character read to prefix
+                int search = dict.searchPrefix(prefix); //Search prefix in dictionary
+                while(search == 2) { //That means still potential children of DLB
+                    prefix.append(BinaryStdIn.readChar(8)); //So append another character from input file
+                    search = dict.searchPrefix(prefix); //Search in dictionary
                 }
 
-                if(search == 1){
-                    BinaryStdOut.write(dict.get(prefix), W);
-                    byteinput = BinaryStdIn.readChar();
-                    prefix.append(byteinput);
+                if(search == 1){ //Value equals 1 means this sequence exists
+                    BinaryStdOut.write(dict.get(prefix), W); //Write that sequence via BinaryStdOut
+                    byteinput = BinaryStdIn.readChar(); //Read another character
+                    prefix.append(byteinput); //Append to prefix
                 }
-                else{
-                    byteinput = prefix.charAt(prefix.length() - 1);
-                    prefix.deleteCharAt(prefix.length() - 1);
-                    BinaryStdOut.write(dict.get(prefix), W);
-                    prefix.append(byteinput);
+                else{ //Value equals 0 means sequence does not exist in dictionary
+                    byteinput = prefix.charAt(prefix.length() - 1); //Save the last character
+                    prefix.deleteCharAt(prefix.length() - 1); //Remove that last character from prefix
+                    BinaryStdOut.write(dict.get(prefix), W); //Write remaining characters
+                    prefix.append(byteinput); //Now put last character back
                 }
 
                 if(code < L) {
-                    dict.add(prefix, code);
+                    dict.add(prefix, code); //Add to dictionary
                     code++;
+                    if( (Math.pow(2, W)) - code < .5)
+                        W++;
                 }
                 prefix.delete(0, prefix.length());
             }
 
             catch(NoSuchElementException e){
+                BinaryStdOut.write(dict.get(prefix), W);
                 BinaryStdOut.write(R, W);
                 BinaryStdOut.close();
                 break;
@@ -73,13 +73,19 @@ public class LZWmod {
         while (true) {
             BinaryStdOut.write(val);
             codeword = BinaryStdIn.readInt(W);
-            if (codeword == R) break;
+            if (codeword == R)
+                break;
             String s = st[codeword];
-            if (i == codeword) s = val + val.charAt(0);   // special case hack
-            if (i < L) st[i++] = val + s.charAt(0);
+            if (i == codeword)
+                s = val + val.charAt(0);   // special case hack
+            if (i < L)
+                st[i++] = val + s.charAt(0);
             val = s;
+            if (Math.pow(2, W) - (i + 1) < 0.1)
+                W++;
         }
         BinaryStdOut.close();
+
     }
 
 
