2023fe06af41dc5adeb7171035d42123f39fc3a0
alans2021 <als432@pitt.edu>
2018-11-15 14:49:13 -0500

    Initial Commit

---
+++ Assignment4/code/Add128.java
@@ -0,0 +1,18 @@
+public class Add128 implements SymCipher{
+
+    public Add128(byte[] b){
+
+    }
+    public byte[] getKey(){
+        return null;
+    }
+
+    public byte [] encode(String S){
+        return null;
+    }
+
+    // Decrypt the array of bytes and generate and return the corresponding String.
+    public String decode(byte [] bytes){
+        return null;
+    }
+}
---
+++ Assignment4/code/Substitute.java
@@ -0,0 +1,18 @@
+public class Substitute implements SymCipher{
+
+    public Substitute(byte[] b){
+
+    }
+    public byte[] getKey(){
+        return null;
+    }
+
+    public byte [] encode(String S){
+        return null;
+    }
+
+    // Decrypt the array of bytes and generate and return the corresponding String.
+    public String decode(byte [] bytes){
+        return null;
+    }
+}
